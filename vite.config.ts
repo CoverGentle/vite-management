65246f28183f939b4da732f917d1e6c78163a1fc
tangshuaibo <tangshuaibo@ghzq.com>
2022-08-30 16:23:44 +0800

    删除按需导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
