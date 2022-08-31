b9278e188179b25cc19f85e9473d4fccd40b8252
tangshuaibo <tangshuaibo@ghzq.com>
2022-08-31 17:40:45 +0800

    提交 new file:   .env.development new file:   .env.production modified:   src/untils/api/index.ts

--- src/untils/api/index.ts
+++ src/untils/api/index.ts
@@ -2,7 +2,7 @@ import request from '../request/request'
 
 
 // 登录
-export const jwtLogin = (data: AdminLoginData)=>{
+export const jwtLogin = (data: AdminLoginData) => {
   return request<AdminLoginRes>({
     url: '/user/login',
     method: 'post',
@@ -10,22 +10,24 @@ export const jwtLogin = (data: AdminLoginData)=>{
   })
 }
 
-// 获取用户列表
-export const getUserList = ()=>{
-  return request<UserList>(
-        {
-          url: '/user',
-          method: 'get',
-        }
-      )
-}
+
 // 注册
-export const registerUser = (data:{username:string,password:string})=>{
+export const registerUser = (data: { username: string, password: string }) => {
   return request(
     {
       url: '/user/register',
       method: 'post',
       data
     }
-    )
+  )
+}
+
+// 获取用户列表
+export const getUserList = () => {
+  return request<UserList>(
+    {
+      url: '/user',
+      method: 'get',
+    }
+  )
 }
