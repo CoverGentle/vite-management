dd984c593312a07ebd59a3853f80c66999212182
tangshuaibo <tangshuaibo@ghzq.com>
2022-09-05 17:53:59 +0800

    首页

--- src/view/pages/firstView.vue
+++ src/view/pages/firstView.vue
@@ -1,14 +1,54 @@
 <template>
-  <div>
-    <h1>首页</h1>
+  <div class="top-select">
+    <el-select class="m-2" placeholder="Select">
+      <el-option />
+    </el-select>
   </div>
+  <div class="contianer">
+    <el-card shadow="always" class="box-card left">
+      <!--  -->
+    </el-card>
+    <el-card shadow="always" class="box-card center">
+    </el-card>
+    <el-card shadow="always" class="box-card right">
+    </el-card>
+  </div>
+  <div></div>
 </template>
 
 <script setup lang='ts'>
-import {reactive, ref, toRefs} from 'vue'
+import { reactive, ref, toRefs } from 'vue'
 
 </script>
 
-<style lang='less' scoped> 
-  
+<style lang='less' scoped>
+  .top-select{
+    padding: 10px;
+  }
+.contianer {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-content: center;
+  justify-content: space-around;
+  height: 90%;
+
+  .box-card {
+    width: 15rem;
+    height: 100%;
+    border-radius: 10px;
+  }
+
+  .left {
+    background: blue;
+  }
+
+  .center {
+    background: green;
+  }
+
+  .right {
+    background: red;
+  }
+}
 </style>
