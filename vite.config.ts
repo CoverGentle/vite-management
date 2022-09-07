5fba3f37006d6d05d7c02d2fbd8873c25019bdd9
tangshuaibo <tangshuaibo@ghzq.com>
2022-09-07 16:03:12 +0800

    vite配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,19 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- 
+  server:{
+    open:true, //vite项目启动时自动打开浏览器
+    hmr:true, //启用热更新
+  },
 
   //生产服务器中基本的公共路径 
   base:'./',
   plugins: [
     vue(),
-  ]
+  ],
+
+  // vite打包
+  build:{
+    
+  }
 })
